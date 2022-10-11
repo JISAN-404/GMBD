60e803c7867049b7729dc31c34e41a240d250365
MR DARK <105338726+JISAN-404@users.noreply.github.com>
2022-10-11 18:49:05 +0600

    Update GMBD.py

--- GMBD.py
+++ GMBD.py
@@ -1,1 +1,26 @@
-import GMBD
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        import GMBD
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        import BDGM
+ 
